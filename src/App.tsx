691f0cec38015291bd4b01a0a248e3aa850b2171
Juan <112003348+juanescan@users.noreply.github.com>
2024-12-11 01:39:57 -0500

    correccion

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React, { ReactElement } from 'react';
 import './App.css';
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { NavBar } from './components/NavBar/NavBar';
 
 function App(): ReactElement {
   return (
     <Box>
-      <Outlet /> {/* Aquí se cargarán los componentes según la ruta */}
-    <Box sx={{ mt: '64px' }}>
-      <NavBar />
-      <Outlet />
+       <Outlet /> {/* Aquí se cargarán los componentes según la ruta */}
+      <Box sx={{ mt: '64px' }}>
+       <NavBar />
+       <Outlet />
+      </Box>
     </Box>
   );
 }
