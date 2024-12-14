9b8983d9e145fcd408371331d64d5c1f0ea7c443
coronado <156589422+Santiago12323@users.noreply.github.com>
2024-12-14 01:15:07 -0500

    implementacion de la api

--- src/components/Loans/Loan/Prestamos.tsx
+++ src/components/Loans/Loan/Prestamos.tsx
@@ -10,11 +10,42 @@ interface PrestamosProps {
 function Prestamos({ onSuccess, onClose, idCopia }: PrestamosProps) {
   const [codigoEstudiante, setCodigoEstudiante] = useState("");
   const [codigoLibro, setCodigoLibro] = useState(idCopia || "");
+  const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("Registrar préstamo:", { codigoEstudiante, codigoLibro });
-    onSuccess();
+
+    // Prepara el objeto JSON para el request
+    const loanRequest = {
+      studentId: codigoEstudiante,
+      copyId: codigoLibro,
+      token: "",
+    };
+
+    try {
+      // Realiza la solicitud POST a la API sin los encabezados de autenticación
+      const response = await fetch(
+        "https://bibliosoftloanback-ahecc7fydjdze0ar.canadacentral-01.azurewebsites.net/loans/requestLoan",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(loanRequest), // Convierte el objeto a JSON
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error en la solicitud. Intenta nuevamente.");
+      }
+
+      const data = await response.json();
+      console.log("Respuesta de la API:", data);
+      onSuccess();
+    } catch (error) {
+      console.error("Error:", error);
+      setError("Ocurrió un error. Por favor, inténtalo de nuevo.");
+    }
   };
 
   const handleClose = () => {
@@ -24,6 +55,8 @@ function Prestamos({ onSuccess, onClose, idCopia }: PrestamosProps) {
   return (
     <div className="container">
       <div className="heading">Registrar un préstamo</div>
+      {error && <div className="error-message">{error}</div>}{" "}
+      {/* Mostrar mensaje de error */}
       <form onSubmit={handleSubmit} className="form">
         <input
           required
